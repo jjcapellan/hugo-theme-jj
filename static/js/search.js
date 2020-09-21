3c9e1b0be245d44d269e494728264366fa1d8ad6
jjcapellan <soycape@hotmail.com>
2020-09-21 22:22:20 +0200

    Added search results layout

---
+++ static/js/search.js
@@ -0,0 +1,27 @@
+const getParam = function () {
+    return window.location.search.split('=')[1];
+}
+
+const getFilter = function (term) {
+    const f = (page) => {
+        if (page.title.toLowerCase().indexOf(term.toLowerCase()) != -1) {
+            return true;
+        }
+        if (page.tags && page.tags.map((tag) => tag.toLowerCase()).indexOf(term.toLowerCase()) != -1) {
+            return true;
+        }
+        if (page.description && page.description.toLowerCase().indexOf(term.toLowerCase()) != -1) {
+            return true;
+        }
+        return false;
+    }
+    return f;
+}
+
+const showResults = function (results, term) {
+    console.log(results.filter(getFilter(term)));
+}
+
+fetch('/search.json')
+    .then(response => response.json())
+    .then(data => showResults(data, getParam()));
