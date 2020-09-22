9a99a159fcf4ba249fe60c1555e7c2432df1052d
jjcapellan <soycape@hotmail.com>
2020-09-22 21:30:26 +0200

    Feature complete: display search results

--- static/js/search.js
+++ static/js/search.js
@@ -1,10 +1,12 @@
+const searchDiv = document.getElementById('search-results');
+
 const getParam = function () {
     return window.location.search.split('=')[1];
 }
 
 const getFilter = function (term) {
     const f = (page) => {
-        if(!page.title){
+        if (!page.title) {
             return false;
         }
         if (page.title.toLowerCase().indexOf(term.toLowerCase()) != -1) {
@@ -25,7 +27,76 @@ const getFilter = function (term) {
 }
 
 const showResults = function (results, term) {
-    console.log(results.filter(getFilter(term)));
+    results = results.filter(getFilter(term));
+
+    if (results.length == 0) {
+        let warning = document.createElement('h3');
+        warning.innerText = 'No results Found';
+        searchDiv.append(warning);
+        return;
+    }
+
+    function addTitle(page) {
+        let h2 = document.createElement('h2');
+        let a = document.createElement('a');
+        a.setAttribute('href', page.uri);
+        a.setAttribute('class', 'color-title');
+        a.innerText = page.title;
+        h2.append(a);
+        searchDiv.appendChild(h2);
+    }
+
+    function addPostData(page) {
+        let div = document.createElement('div');
+        div.setAttribute('class', 'post-data');
+
+        function addDate() {
+            let span = document.createElement('span');
+            span.innerText = page.date;
+            div.appendChild(span);
+        }
+
+        function addTagIcon() {
+            let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            let use = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+            svg.setAttribute('class', 'icon');
+            use.setAttribute('href', '/imgs/jj-icons.svg#tag');
+            svg.appendChild(use);
+            div.appendChild(svg);
+        }
+
+        function addTags(){
+            if(page.tags == null){
+                return;
+            }
+            let tags = page.tags;
+            tags.forEach(tag => {
+                let a = document.createElement('a');
+                a.setAttribute('href',`/tags/${tag}`);
+                a.setAttribute('class', 'post-tag');
+                a.innerText = `#${tag}`;
+                div.appendChild(a);
+            });            
+        }
+
+        addDate();
+        addTagIcon();
+        addTags();
+        searchDiv.appendChild(div);
+    }
+
+    let msg = `${results.length} item${(results.length>1)?'s':''} found:`;
+    let h3 = document.createElement('h3');
+    h3.innerText = msg;
+    searchDiv.appendChild(h3);
+
+    results.forEach(page => {
+        addTitle(page);
+        addPostData(page);
+        let hr = document.createElement('hr');
+        searchDiv.appendChild(hr);
+    });
+
 }
 
 fetch('/search.json')
