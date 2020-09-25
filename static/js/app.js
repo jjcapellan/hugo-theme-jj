7da547f407eac84846b39c634b97b523b71528d1
jjcapellan <soycape@hotmail.com>
2020-09-25 21:21:57 +0200

    Fix: textbox search wont close on lostfocus

--- static/js/app.js
+++ static/js/app.js
@@ -6,13 +6,12 @@ const navHeader = document.getElementById('header-nav');
 const formSearch = document.getElementById('form-search');
 
 let btSearchIsActive = false;
+let tbIsLostFocus = false;
 
-const btSearchHandler = function () {
+const btSearchHandler = function (event) {
+    console.log(event);    
     if (btSearchIsActive) {
-        tbSearch.style.width = "0";
-        tbSearch.style.opacity = "0";
-        btSearchIsActive = false;
-        navHeader.style.opacity = "1";
+        closeSearch(event);
         if (tbSearch.value) {
             formSearch.submit();
         }
@@ -21,7 +20,23 @@ const btSearchHandler = function () {
         tbSearch.style.width = "250px";
         navHeader.style.opacity = "0";
         btSearchIsActive = true;
+        tbSearch.focus();
+    }
+}
+
+const closeSearch = function(event){
+    if(event.target == tbSearch && btSearch == formSearch.querySelector(':hover')){
+        return;
+    }
+    tbSearch.style.width = "0";
+    tbSearch.style.opacity = "0";
+    navHeader.style.opacity = "1";
+    btSearchIsActive = false;
+    if(event.target == tbSearch){
+        tbSearch.value = "";
     }
 }
 
 btSearch.addEventListener('click', btSearchHandler);
+
+tbSearch.addEventListener('focusout', closeSearch);
