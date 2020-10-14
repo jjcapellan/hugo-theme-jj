2040b28b990f3cb8b7ee43faa5cc90ff80fe98f3
jjcapellan <soycape@hotmail.com>
2020-10-14 17:49:58 +0200

    Feature search: optional search

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,42 +1,48 @@
 //// Search button functionality
 ////
-const btSearch = document.getElementById('btsearch');
-const tbSearch = document.getElementById('tbsearch');
-const navHeader = document.getElementById('header-nav');
-const formSearch = document.getElementById('form-search');
+function activateSearch() {
+    const btSearch = document.getElementById('btsearch');
+    const tbSearch = document.getElementById('tbsearch');
+    const navHeader = document.getElementById('header-nav');
+    const formSearch = document.getElementById('form-search');
 
-let btSearchIsActive = false;
-let tbIsLostFocus = false;
+    let btSearchIsActive = false;
+    let tbIsLostFocus = false;
 
-const btSearchHandler = function (event) {
-    if (btSearchIsActive) {
-        closeSearch(event);
-        if (tbSearch.value) {
-            formSearch.submit();
+    const btSearchHandler = function (event) {
+        if (btSearchIsActive) {
+            closeSearch(event);
+            if (tbSearch.value) {
+                formSearch.submit();
+            }
+        } else {
+            tbSearch.style.opacity = '1';
+            tbSearch.style.zIndex = '10';
+            btSearchIsActive = true;
+            tbSearch.focus();
         }
-    } else {
-        tbSearch.style.opacity = '1';
-        tbSearch.style.zIndex='10';
-        btSearchIsActive = true;
-        tbSearch.focus();
     }
+
+    const closeSearch = function (event) {
+        if (event.target == tbSearch && btSearch == formSearch.querySelector(':hover')) {
+            return;
+        }
+        tbSearch.style.opacity = '0';
+        tbSearch.style.zIndex = '-10';
+        btSearchIsActive = false;
+        if (event.target == tbSearch) {
+            tbSearch.value = '';
+        }
+    }
+
+    btSearch.addEventListener('click', btSearchHandler);
+
+    tbSearch.addEventListener('blur', closeSearch);
 }
 
-const closeSearch = function (event) {
-    if (event.target == tbSearch && btSearch == formSearch.querySelector(':hover')) {
-        return;
-    }
-    tbSearch.style.opacity = '0';
-    tbSearch.style.zIndex='-10';
-    btSearchIsActive = false;
-    if (event.target == tbSearch) {
-        tbSearch.value = '';
-    }
+if (document.getElementById('btsearch')) {
+    activateSearch();
 }
-
-btSearch.addEventListener('click', btSearchHandler);
-
-tbSearch.addEventListener('blur', closeSearch);
 
 
 
